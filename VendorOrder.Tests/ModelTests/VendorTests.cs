a6aa9ec7319c6d530b2339c2217798390fc95ad1
Sue Roberts <suerobertstx@yahoo.com>
2022-07-14 18:38:03 -0500

    Pass: Test 2: GetName

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -21,19 +21,19 @@ namespace VendorOrder.Tests
         Assert.AreEqual(typeof(Vendor), newVendor.GetType());
       }
 
-    // [TestMethod]
-    // public void GetName_ReturnsName_String()
-    // {
-    //   //Arrange
-    //   string name = "Test Vendor";
-    //   Vendor newVendor = new Vendor(name);
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
 
-    //   //Act
-    //   string result = newVendor.Name;
+      //Act
+      string result = newVendor.Name;
 
-    //   //Assert
-    //   Assert.AreEqual(name, result);
-    // }
+      //Assert
+      Assert.AreEqual(name, result);
+    }
 
     // [TestMethod]
     // public void GetId_ReturnsVendorId_Int()
