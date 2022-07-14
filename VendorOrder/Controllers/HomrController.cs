2915384815515403bfaa5ae857c7ab194463c0fb
Sue Roberts <suerobertstx@yahoo.com>
2022-07-14 12:59:51 -0500

    Update: Folers And Files name.

--- VendorOrder/Controllers/HomrController.cs
+++ VendorOrder/Controllers/HomrController.cs
@@ -2,19 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace VendorOrder.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
-      [Route("/favorite_photos")]
-      public ActionResult FavoritePhotos()
-      {
-        return View();
-      }
+      return View();
     }
+  }
 }
