c395a23329154266365caa945b0a9bd16d5da6b1
Sue Roberts <suerobertstx@yahoo.com>
2022-07-14 19:05:48 -0500

    Pass: Test 4:GetAll

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -46,22 +46,17 @@ namespace VendorOrder.Tests
       Assert.AreEqual(1, result);
     }
 
-    // [TestMethod]
-    // public void GetAll_ReturnsAllVendorObjects_VendorList()
-    // {
-    //   //Arrange
-    //   string name01 = "Work";
-    //   string name02 = "School";
-    //   Vendor newVendor1 = new Vendor(name01);
-    //   Vendor newVendor2 = new Vendor(name02);
-    //   List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
-
-    //   //Act
-    //   List<Vendor> result = Vendor.GetAll();
-
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Store1";
+      string name02 = "Store2";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
 
     // [TestMethod]
     // public void Find_ReturnsCorrectVendor_Vendor()
