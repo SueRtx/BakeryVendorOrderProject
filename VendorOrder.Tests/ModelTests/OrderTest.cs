d07880bf1227f60fc3484ef9f0cf90cd782f9366
Sue Roberts <suerobertstx@yahoo.com>
2022-07-14 19:51:53 -0500

    Pass: Test 11: GetAll_ReturnEmptyList

--- VendorOrder.Tests/ModelTests/OrderTest.cs
+++ VendorOrder.Tests/ModelTests/OrderTest.cs
@@ -54,18 +54,13 @@ namespace VendorOrder.Tests
       Assert.AreEqual(date, result);
     }
 
-    // [TestMethod]
-    // public void GetAll_ReturnsEmptyList_OrderList()
-    // {
-    //   // Arrange
-    //   List<Order> newList = new List<Order> { };
-
-    //   // Act
-    //   List<Order> result = Order.GetAll();
-
-    //   // Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
 
     // [TestMethod]
     // public void GetAll_ReturnsOrders_OrderList()
